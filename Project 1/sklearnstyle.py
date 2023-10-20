b5a20cbc91a44a5ed8b5445aa5a28421f73f8b55
faverog <faverog@uwindsor.ca>
2023-10-19 22:24:11 -0400

    some test

--- Project 1/sklearnstyle.py
+++ Project 1/sklearnstyle.py
@@ -4,30 +4,28 @@ from NestedCrossValidation import NestedCV
 import pandas as pd
 
 # Load the data
-mushroom_set = pd.read_csv('Mushroom.csv')
-mushroom_headers = ['Poisonous', 'Cap-shape', 'Cap-surface', 'Cap-color', 'Bruises', 'Odor', 'Gill-attachment',  'Gill-spacing', 'Gill-size', 'Gill-color', 'Stalk-color-below-ring', 'Class']
-mushroom_set.columns = mushroom_headers
+hepatitis_set = pd.read_csv('Hepatits.csv')
+hepatitis_headers = ['Ascites', 'Varices', 'Bilirubin', 'Alk Phosphate', 'Sgot', 'Albumin', 'Protime', 'Histology', 'Class']
+hepatitis_set.columns = hepatitis_headers
 
 # Get the labels from last column
-X = mushroom_set.iloc[:, :-1]
-Y = mushroom_set.iloc[:, -1]
+X = hepatitis_set.iloc[:, :-1]
+Y = hepatitis_set.iloc[:, -1]
 
-# Create L1 model
-l1_model = LogisticRegression(penalty='l1', solver='liblinear')
-l1_accuracy = NestedCV(outer_folds=10).k_fold_cross_validation2(l1_model, X, Y)
-
-# Create L2 model
-l2_model = LogisticRegression(penalty='l2', solver='liblinear')
-l2_accuracy = NestedCV(outer_folds=10).k_fold_cross_validation2(l2_model, X, Y)
+# Remove class feature from headers
+hepatitis_headers = hepatitis_headers[:-1]
 
 # Create no regularization model
-no_reg_model = LogisticRegression(penalty='none', solver='lbfgs')
-no_reg_accuracy = NestedCV(outer_folds=10).k_fold_cross_validation2(no_reg_model, X, Y)
+model = LogisticRegression(penalty='l2', solver='liblinear')
 
-# Print the accuracies
-print("L1 Accuracy: ", l1_accuracy)
-print("L2 Accuracy: ", l2_accuracy)
-print("No Regularization Accuracy: ", no_reg_accuracy)
+# Find accuracy of no reg model
+nested_cv = NestedCV(outer_folds=10)
+
+# Evaluate the candidate models using a 10-fold cross validation
+candidate_scores = nested_cv.k_fold_cross_validation(model, X, Y)
+
+# Print the average accuracy for the model
+print("No regularization accuracy: ", candidate_scores)
 
 
 
